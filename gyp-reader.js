eee30c57e04fb132529306966eaf71ac6d455bd8
jk lee <ntoskrnl7@gmail.com>
2019-11-09 21:19:24 +0900

    python 3 supported

--- gyp-reader.js
+++ gyp-reader.js
@@ -1,11 +1,15 @@
 // you must growl every time you use this module.
 module.exports = gr
 
+var compareVersions = require('compare-versions');
+var exec = require('child_process').execSync;
+var python_version = exec('python -c "import platform; print(platform.python_version())"').toString().trim();
+
 var spawn = require("child_process").spawn
 , pyprog = "import sys, ast, json;"
          + "c = open(sys.argv[1]).read();"
          + "d = ast.literal_eval(c);"
-         + "print json.dumps(d)"
+         + (compareVersions(python_version, "3.0") >=0 ? "print(json.dumps(d))" : "print json.dumps(d)")
 , StringDecoder = require("string_decoder").StringDecoder
 
 function gr (file, cb) {
